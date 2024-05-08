b02ef98f9068b736f482eafeb0b88a9ff68599c8
David Miklos <miklosdavid0210@gmail.com>
2024-05-08 10:43:35 +0200

    separate [request, response] from profile-spec, add openapi-merger

--- src/main/java/poc/uni/buddy/controller/ProfileController.java
+++ src/main/java/poc/uni/buddy/controller/ProfileController.java
@@ -6,7 +6,11 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import poc.uni.buddy.api.ProfileApi;
+import poc.uni.buddy.model.ProfileRequest;
+import poc.uni.buddy.model.ProfileResponse;
 import poc.uni.buddy.repository.model.Profile;
+import poc.uni.buddy.service.ProfileMapper;
 import poc.uni.buddy.service.ProfileService;
 
 import java.util.UUID;
@@ -15,42 +19,41 @@ import java.util.UUID;
 @RequiredArgsConstructor
 @RequestMapping("api/v1")
 @RestController
-public class ProfileController {
+public class ProfileController implements ProfileApi {
+
+    ProfileMapper profileMapper = ProfileMapper.INSTANCE;
 
     private final ProfileService profileService;
 
-    @GetMapping("welcome")
-    public String welcome() {
-        return "greetings from uni-buddy";
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseEntity<String> welcome() {
+        return ResponseEntity.ok("greetings fom uni-buddy");
     }
 
     @ResponseStatus(HttpStatus.CREATED)
-    @PostMapping("profile/create")
-    public ResponseEntity<Profile> createProfile(@RequestBody Profile profile) {
-        Profile createdProfile = profileService.createProfile(profile);
-        return ResponseEntity.status(HttpStatus.CREATED).body(createdProfile);
+    public ResponseEntity<ProfileResponse> createProfile(@RequestBody ProfileRequest profileRequest) {
+        Profile createdProfile = profileService.createProfile(profileMapper.mapToProfile(profileRequest));
+        return ResponseEntity.status(HttpStatus.CREATED).body(profileMapper.mapToProfileResponse(createdProfile));
     }
 
     @ResponseStatus(HttpStatus.OK)
-    @GetMapping("profile")
-    public ResponseEntity<Profile> getProfileByNickname(@RequestParam String nickname) {
-        return ResponseEntity.ok(profileService.getProfileByNickname(nickname));
+    public ResponseEntity<ProfileResponse> getProfileByNickname(@RequestParam String nickname) {
+        return ResponseEntity.ok(profileMapper.mapToProfileResponse(profileService.getProfileByNickname(nickname)));
     }
 
     @ResponseStatus(HttpStatus.OK)
     @GetMapping("profile/{id}")
-    public ResponseEntity<Profile> getProfileById(@PathVariable UUID id) {
-        return ResponseEntity.ok(profileService.getProfileById(id));
+    public ResponseEntity<ProfileResponse> getProfileById(@PathVariable UUID id) {
+        return ResponseEntity.ok(profileMapper.mapToProfileResponse(profileService.getProfileById(id)));
     }
 
     @ResponseStatus(HttpStatus.OK)
-    @PutMapping("profile/{id}")
-    public ResponseEntity<Profile> updateProfile(@PathVariable UUID id, @RequestBody Profile profile) {
-        return ResponseEntity.ok(profileService.updateProfile(id, profile));
+    public ResponseEntity<ProfileResponse> updateProfile(@PathVariable UUID id, @RequestBody ProfileRequest profileRequest) {
+        return ResponseEntity.ok(profileMapper.mapToProfileResponse(
+                profileService.updateProfile(id, profileMapper.mapToProfile(profileRequest))));
     }
 
-    @ResponseStatus(HttpStatus.OK)
-    @DeleteMapping("profile/delete/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     public ResponseEntity<Void> deleteProfile(@PathVariable UUID id) {
         try {
             profileService.deleteProfile(id);
--- src/main/java/poc/uni/buddy/service/ProfileMapper.java
+++ src/main/java/poc/uni/buddy/service/ProfileMapper.java
@@ -4,12 +4,18 @@ import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
 import org.mapstruct.factory.Mappers;
+import poc.uni.buddy.model.ProfileRequest;
+import poc.uni.buddy.model.ProfileResponse;
 import poc.uni.buddy.repository.model.Profile;
 
 @Mapper
 public interface ProfileMapper {
 
     ProfileMapper INSTANCE = Mappers.getMapper(ProfileMapper.class);
+
+    Profile mapToProfile(ProfileRequest profileRequest);
+
+    ProfileResponse mapToProfileResponse(Profile profile);
 
     @Mapping(target = "id", ignore = true)
     void updateExistingProfile(@MappingTarget Profile existingProfile, Profile newProfile);
